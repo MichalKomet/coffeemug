19cb404d9b1f5d25630b09649fd271c913990901
Michał Gwoździej <gwozdziej.michal@gmail.com>
2025-01-10 16:37:54 +0100

    add getAllProducts endpoint

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { connectToDB } from './mongoConnection.js';
+import productsRoutes from "./src/routes/productsRoutes.js";
 
 const app = express();
 app.use(express.json());
 
 const SERVER_PORT = process.env.SERVER_PORT;
+
+app.use('/products', productsRoutes);
 
 connectToDB().then(() => {
     app.listen(SERVER_PORT, () => {
