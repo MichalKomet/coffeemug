331fd21217483877e6bae2f0e20f1d67ed6bdc9f
Michał Gwoździej <gwozdziej.michal@gmail.com>
2025-01-10 14:01:43 +0100

    database connection

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import { connectToDB } from './mongoConnection.js';
 
 const app = express();
+app.use(express.json());
 
 const SERVER_PORT = process.env.SERVER_PORT;
 
-app.listen(SERVER_PORT, () => console.log(`server is listening on port ${SERVER_PORT}`));
+connectToDB().then(() => {
+    app.listen(SERVER_PORT, () => {
+        console.log(`App listening on port ${SERVER_PORT}`);
+    });
+});
