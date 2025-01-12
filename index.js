553c43a1941d459d1aa51c8f740f9e1059828560
Michał Gwoździej <gwozdziej.michal@gmail.com>
2025-01-12 22:49:56 +0100

    implement global error handling

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const SERVER_PORT = process.env.SERVER_PORT;
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    if (err.statusCode) {
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            ...(err.errors && { errors: err.errors })
+        });
+    }
+
+    res.status(500).json({
+        success: false,
+        message: 'Internal Server Error'
+    });
+});
+
 connectToDB().then(() => {
     app.listen(SERVER_PORT, () => {
         console.log(`App listening on port ${SERVER_PORT}`);
